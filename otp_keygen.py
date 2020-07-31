0b2510a2aaa03ca07a98faee7ea79270987f0706
Daniel Dimitric <daniel.dimitric91@gmail.com>
2020-08-01 00:47:52 +0200

    name refactor

--- otp_keygen.py
+++ otp_keygen.py
@@ -4,10 +4,10 @@ import sys
 
 def keygen():
     key_file_name = sys.argv[1]
-    key_lenght = int(sys.argv[2])
+    key_len = int(sys.argv[2])
     if not key_file_name[-4:] == ".txt":
         key_file_name = key_file_name + ".txt"
-    hex_key = secrets.token_hex(key_lenght)
+    hex_key = secrets.token_hex(key_len)
     bit_len = len(hex_key)*4
     bit_key = str(bin(int(hex_key, 16)))[2:].zfill(bit_len)
     key_file = open(key_file_name, "w")
